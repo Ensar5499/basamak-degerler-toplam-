c49ee55d7de32d5abe51c046922e746afdb475f8
Ensar5499 <ensarkulaksiz54@gmail.com>
2024-11-20 17:19:30 +0300

    Add files via upload
    girilen sayının basamak değerlerini toplar.

---
+++ basamak değerleri toplamı.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Bir sayı girin");
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            while (sayi > 0)
+
+            {
+                toplam += sayi % 10;
+                sayi /= 10;
+            }
+
+            Console.WriteLine("basamak değerlerinin toplamı" + toplam);
+            Console.ReadLine();
+
+
+        }
+    }
+}
